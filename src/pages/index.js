525909f67ae9a67c6494f0f77c2a47bfd3128971
Matt Young <youngness@gmail.com>
2020-03-21 13:36:30 +0000

    Test change to see if deploy works (#1)
    Testing.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Testing Github based Deploy!</h1>
+    <p>This was deployed via github action, awesome!</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
